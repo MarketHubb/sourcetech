55c778a365f264033a62f57a56060f1ed64576ef
Chris Hubbard <chris@markethubb.com>
2019-09-12 10:51:07 -0400

    API customizations for Zoho live chat widget

--- wp-content/themes/source-tech/footer.php
+++ wp-content/themes/source-tech/footer.php
@@ -53,5 +53,20 @@
 var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:"00c1181641a8e8a370b3731ece5fb9e8aaf8fb161310d62c4552975ade569d50", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
 </script>
 
+<!-- Zoho Customizations -->
+<script>
+
+$zoho.salesiq.ready=function(embedinfo)
+
+{
+   $zoho.salesiq.chatbutton.texts([["Hi! Here to browse, or", "can I get you a quote?"], ["Hi! Here to browse, or", "can I get you a quote?", "Offline"]]);
+   $zoho.salesiq.floatwindow.onlinetitle('Live Support');
+   $zoho.salesiq.floatwindow.offlinetitle('Live Support');
+   $zoho.salesiq.floatwindow.fields({"offline":"name,email"});
+}
+
+
+</script>
+
 </body>
 </html>
