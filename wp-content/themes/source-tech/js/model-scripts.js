9350baae39365e23b704e97567805d627704ce85
Chris Hubbard <chris@markethubb.com>
2019-10-04 14:27:19 -0400

    Design and development of server model page

--- wp-content/themes/source-tech/js/model-scripts.js
+++ wp-content/themes/source-tech/js/model-scripts.js
@@ -5,37 +5,37 @@
     
     // https://www.viralpatel.net/dynamically-shortened-text-show-more-link-jquery/
     
-    	var showChar = 200;
-    	var ellipsestext = "...";
-    	var moretext = "more";
-    	var lesstext = "less";
-    	$('.model-page-description').each(function() {
-    		var content = $(this).html();
+    	// var showChar = 200;
+    	// var ellipsestext = "...";
+    	// var moretext = "more";
+    	// var lesstext = "less";
+    	// $('.model-page-description').each(function() {
+    	// 	var content = $(this).html();
 
-    		if(content.length > showChar) {
+    	// 	if(content.length > showChar) {
 
-    			var c = content.substr(0, showChar);
-    			var h = content.substr(showChar-1, content.length - showChar);
+    	// 		var c = content.substr(0, showChar);
+    	// 		var h = content.substr(showChar-1, content.length - showChar);
 
-    			var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+    	// 		var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
 
-    			$(this).html(html);
-    		}
+    	// 		$(this).html(html);
+    	// 	}
 
-    	});
+    	// });
 
-    	$(".morelink").click(function(){
-    		if($(this).hasClass("less")) {
-    			$(this).removeClass("less");
-    			$(this).html(moretext);
-    		} else {
-    			$(this).addClass("less");
-    			$(this).html(lesstext);
-    		}
-    		$(this).parent().prev().toggle();
-    		$(this).prev().toggle();
-    		return false;
-    	});
+    	// $(".morelink").click(function(){
+    	// 	if($(this).hasClass("less")) {
+    	// 		$(this).removeClass("less");
+    	// 		$(this).html(moretext);
+    	// 	} else {
+    	// 		$(this).addClass("less");
+    	// 		$(this).html(lesstext);
+    	// 	}
+    	// 	$(this).parent().prev().toggle();
+    	// 	$(this).prev().toggle();
+    	// 	return false;
+    	// });
     	
     	// Image thumbnail gallery
     	$('#model-page-image-container .image-thumb-container .col img').on('click', function(){
