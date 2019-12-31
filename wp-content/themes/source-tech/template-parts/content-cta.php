7efbded815d1839c93113e16ce74023f7cf7787f
Chris Hubbard <chris@markethubb.com>
2019-12-31 13:56:09 -0500

    Global CTA module

---
+++ wp-content/themes/source-tech/template-parts/content-cta.php
@@ -0,0 +1,13 @@
+<!-- Section: CTA Card -->
+<div class="card border-light cta-card text-right shadow">
+    <div class="card-body cta-header bg-orange">
+        <h3 class="card-title text-white mb-0">Get a Custom Quote Today</h3>
+        <p class="card-text"></p>
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">Live Chat <i class="fas fa-comments-alt"></i></li>
+        <li class="list-group-item"><a href="tel:800-932-0657">800-932-0657</a> <i class="fas fa-phone"></i></li>
+        <li class="list-group-item"><a href="mailto:info@source-tech.net">info@source-tech.net</a> <i
+                    class="fas fa-envelope-open-text"></i></li>
+    </ul>
+</div>
