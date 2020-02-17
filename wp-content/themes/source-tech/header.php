3b0bbf90bfdcda4c820d461d957d8a66252e3fdc
Chris Hubbard <chris@markethubb.com>
2020-02-17 13:35:07 -0500

    New: move ahrefs domain verification in header

--- wp-content/themes/source-tech/header.php
+++ wp-content/themes/source-tech/header.php
@@ -24,6 +24,7 @@
 
 	<?php wp_head(); ?>
 
+
     <!-- ahrefs domain verification -->
     <meta name="ahrefs-site-verification" content="b1123fd872bc8c7860c22eadc8959120a93e9d7d4a3999dd64a3141842640706">
 	
