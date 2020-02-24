862406fdef2b6a34ef88dde9d196d40c1ad4ce2d
Chris Hubbard <chris@markethubb.com>
2020-02-24 15:26:17 -0500

    Add testimonials to single products page

---
+++ wp-content/themes/source-tech/template-parts/products/testimonials.php
@@ -0,0 +1,9 @@
+<div class="row model-page-testimonial">
+    <div class="col">
+        <blockquote class="blockquote text-center">
+            <i class="fal fa-quote-right"></i>
+            <p class="mb-0">"Abundant supply. Fastest service. Lowest prices. Proven in multiple deals. Our #1 choice"</p>
+            <footer class="blockquote-footer"><cite title="Source Title">UMMC Tidwell</cite></footer>
+        </blockquote>
+    </div>
+</div>
