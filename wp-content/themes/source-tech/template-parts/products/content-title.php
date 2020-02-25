b17102189862f3d2c0b90017a3c3b0f4ea58c706
Chris Hubbard <chris@markethubb.com>
2020-02-25 10:30:58 -0500

    Single product page subtitle

--- wp-content/themes/source-tech/template-parts/products/content-title.php
+++ wp-content/themes/source-tech/template-parts/products/content-title.php
@@ -2,12 +2,13 @@
 $tags = get_query_var('tags');
 $title = ($post->post_type == 'servers') ? get_the_title() . ' Servers' : get_the_title();
 $subtitle_field = 'global_' . $post->post_type . '_subtitle';
+$subtitle = replace_product_variable_in_string(get_field($subtitle_field, 'option'), $post->ID);
 ?>
 
     <div class="row model-page-title mt-4">
         <div class="col-sm-12 col-md-6 col-lg-8">
             <h1 class="page-title"><?php echo $title; ?></h1>
-            <h6 class="server-title-abstract mb-3"><?php the_field($subtitle_field, 'option'); ?></h6>
+            <h6 class="server-title-abstract mb-3"><?php echo $subtitle; ?></h6>
             <?php
             foreach ($tags as $tag => $value) {
                 if ($tag != 'title') {
