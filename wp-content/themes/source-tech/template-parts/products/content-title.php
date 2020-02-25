1503eca50eeaeec1cd13aefeb7d0c89272f8d64d
Chris Hubbard <chris@markethubb.com>
2020-02-25 10:20:04 -0500

    Prevent product title from being output as tag on single product

--- wp-content/themes/source-tech/template-parts/products/content-title.php
+++ wp-content/themes/source-tech/template-parts/products/content-title.php
@@ -9,16 +9,10 @@ $subtitle_field = 'global_' . $post->post_type . '_subtitle';
             <h1 class="page-title"><?php echo $title; ?></h1>
             <h6 class="server-title-abstract mb-3"><?php the_field($subtitle_field, 'option'); ?></h6>
             <?php
-            if( have_rows('post_product_tags') ):
-                $tags = '';
-                while ( have_rows('post_product_tags') ) : the_row();
-                    $tags .= '<span class="badge badge-pill badge-primary model-page-tags">' . get_sub_field('post_product_tags_tag') . '</span>';
-                endwhile;
-                echo $tags;
-            endif;
-
             foreach ($tags as $tag => $value) {
-                $tag_items .= '<span class="badge badge-pill badge-primary model-page-tags">' . $value . '</span>';
+                if ($tag != 'title') {
+                    $tag_items .= '<span class="badge badge-pill badge-primary model-page-tags">' . $value . '</span>';
+                }
             }
 
             echo $tag_items;
