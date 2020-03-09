df8a702ef49d5189d41b0c0d4f9248f234214f5c
Chris Hubbard <chris@markethubb.com>
2020-03-09 15:16:24 -0400

    Update 'Specs' tab to 'Models' on Networking pages

--- wp-content/themes/source-tech/template-parts/products/tab-headings.php
+++ wp-content/themes/source-tech/template-parts/products/tab-headings.php
@@ -5,7 +5,8 @@
                 <a class="nav-link active" href="#server" data-toggle="tab" role="tab"><i class="fal fa-server fa-lg"></i>Overview</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#specs" data-toggle="tab" role="tab"><i class="fal fa-file-alt fa-lg"></i>Specs</a>
+                <?php $specs_tab = (get_post_type() === 'servers') ? 'Specs' : 'Models'; ?>
+                <a class="nav-link" href="#specs" data-toggle="tab" role="tab"><i class="fal fa-file-alt fa-lg"></i><?php echo $specs_tab; ?></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#faq" data-toggle="tab" role="tab"><i class="fal fa-question-circle fa-lg"></i>FAQs</a>
